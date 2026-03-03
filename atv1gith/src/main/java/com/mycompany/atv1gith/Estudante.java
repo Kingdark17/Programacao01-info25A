338985e64608ac71830ece19d70f88bcdb9ea556
aluno <aluno@lab01-30>
2026-03-03 10:34:19 -0300

    Estuante atv gith

---
+++ atv1gith/src/main/java/com/mycompany/atv1gith/Estudante.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atv1gith;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class Estudante {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        
+        
+        
+        
+    }
+    
+}
