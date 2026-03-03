e6ebd3391ffce200c0d6d1637972c69a80f037d8
aluno <aluno@lab01-30>
2026-03-03 10:20:05 -0300

    pimer atv gith

---
+++ atv1gith/src/main/java/com/mycompany/atv1gith/Atv1gith.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atv1gith;
+
+/**
+ *
+ * @author aluno
+ */
+public class Atv1gith {
+
+    public static void main(String[] args) {
+        System.out.println("fala fiot");
+    }
+}
