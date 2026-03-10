1c12b434016714cd59f8f3e2fb61c085c1b25c73
aluno <aluno@lab01-29>
2026-03-10 10:35:12 -0300

    Nova class

---
+++ PedroHenrique/src/main/java/ifc/pedrohenrique/SistemaIFC.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifc.pedrohenrique;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class SistemaIFC {
+    public static void main(String[] args) {
+        Estudante Pedro = new Estudante ();
+        Pedro.getNome();
+        Pedro.obterIdade(new Date());
+        Pedro.genero = 'm';
+        
+        System.out.println("Hellow Word");
+    
+    
+    
+    
+    }
+}
